1c5a578eef97f076f9934352f09fc7cae2138d26
Сергеич <seryjmaslouv@gmail.com>
2026-05-15 12:39:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function checkForm(el){
+    
+    event.preventDefault();
+    var name = el.name.value;
+    var pass = el.password.value;
+    var repass = el.repassword.value;
+
+    var fail = "";
+
+    if(name == "" || pass == "" || repass == ""){
+        fail = "Заполните все поля";
+    }
+    else if(name.length < 2 || name.length > 10){
+        fail = "Введите корректное имя";
+    }
+    else if(pass != repass){
+        fail = "Пароли не совпадают";
+    }
+
+    if(fail != ""){
+        document.getElementById('error').innerHTML = fail;
+    }
+    else{
+        alert("Всё окей");
+        window.location = 'index1.html';
+    }
+}
